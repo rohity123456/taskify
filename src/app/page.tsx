b1e082c63b2a74569359b48f7486abd9d2a01709
Rohit Yadav <rohity123456@gmail.com>
2024-07-03 08:56:45 +0530

    removed error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const session = await auth();
-  throw new Error('test error');
   if (!session?.user) redirect('/signin');
   return <TasksDashboard />;
 }
